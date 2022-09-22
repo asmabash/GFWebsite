3db84eb8bdced2829fe589faa55e9de4ac02e8e0
Asma Bashamakh <asmabashamakh1@gmail.com>
2022-09-22 21:53:39 +0300

    fixed lang images on phone

--- show-restaurant-card.php
+++ show-restaurant-card.php
@@ -3,5 +3,6 @@
 <?php
 if(isset($_GET['card-lang'])){
     $cardLang = $_GET['card-lang'];
-    echo "<div class=\"center-img\"><img class=\"center-img\" src=\"images/{$cardLang}-gluten-free-restaurant-card.png\" alt=\"بطاقة تعريف سيلياك لغة {$cardLang}\"></div>";
+    echo "<div class=\"lang-img\"><img style=\"max-width: 100%;\" src=\"images/{$cardLang}-gluten-free-restaurant-card.png\" alt=\"بطاقة تعريف سيلياك لغة {$cardLang}\"></div>";
 }
+include "includes/footer.php";?>
